0759837fb5e67179f35a5c67b68cebcc08b64f05
kwong09 <wongkayla09@gmail.com>
2025-06-15 02:44:32 +0000

    jumping game end, obstacle, and jumping mechanism working

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 var homeScreen = document.querySelector(".homeScreen");
 var jumpingGameScreen = document.querySelector(".jumpingGameScreen");
+var jumpingCharacter = document.getElementById("jumpingCharacter");
 var screen;
 function invisible(screen) {
-    screen.classList.add("invisible");
-    screen.classList.remove("visible");
+    screen.style.display = "none";
 }
 
 function visible(screen) {
-    screen.classList.add("visible");
-    screen.classList.remove("invisible");
+    screen.style.display = "block";
 }
+
+function jumpingCharacterAni(character) {
+    if (character.classList == "jumpingCharacterAni") {return}
+    character.classList.add("jumpingCharacterAni");
+    setTimeout(function() {
+        character.classList.remove("jumpingCharacterAni")
+    }, 500);
+}
+
+var jumpingPoints = setInterval(function() {
+    let jumpingCharacterTop = parseInt(window.getComputedStyle(jumpingCharacter).getPropertyValue("top"));
+    let jumpingObstacle1Left = parseInt(window.getComputedStyle(jumpingObstacle1).getPropertyValue("left"));
+    if (jumpingCharacterTop>=460 && jumpingObstacle1Left>=30 && jumpingObstacle1Left<=100) {
+        jumpingObstacle1.style.animation = "none";
+        alert("Game over.");
+        jumpingObstacle1.style.animation = "jumpingObstacleAni 1.6s infinite linear";
+    }
+}, 10);
